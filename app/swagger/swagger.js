b3b8e12d2bd15976ad52fe39f78cf162c1e57c34
Eight-Corner <corner3499@gmail.com>
2022-06-06 20:42:20 +0900

    fix

--- app/swagger/swagger.js
+++ app/swagger/swagger.js
@@ -1,13 +1,16 @@
 const swaggerUi = require("swagger-ui-express")
 const swaggerJSDoc = require('swagger-jsdoc');
+const {getConfig, isDev} = require("../config/db.config");
 
+let key = 'HOST'
+console.log(`isDev: ${isDev}`.yellow, `config: ${getConfig(key)}`.yellow)
 var swaggerDefinition = {
     info : { // 정보 작성
         version: "1.0.0",
         title: "Diary API DOCS",
         description: "Diary Web API 문서입니다. ",
     },
-    host : "localhost:8081", // base-url
+    host : getConfig(key), // base-url
     basePath : "/api" // base path
 };
 
