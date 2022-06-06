37258899e8a05de36140e910ff55397a5443bcd5
Eight-Corner <corner3499@gmail.com>
2022-06-06 12:32:11 +0900

    config set

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,28 @@
+// todo:: true : dev, false: prod
+let isDev = true;
+
+let config = {
+    dev: {
+        HOST: 'localhost',
+        USER: 'root',
+        PASSWORD: 'Thiskim12!@',
+    },
+    prod: {
+        HOST: 'dev-diary.cc4cswhxpsvw.us-east-1.rds.amazonaws.com',
+        USER: 'admin',
+        PASSWORD: 'rodtmxj.123'
+    },
+}
+
+function getConfig(key) {
+    return isDev ? config.dev[key] : config.prod[key];
+}
+
+//    "host": "ec2-107-23-80-232.compute-1.amazonaws.com",
 module.exports = {
+    isDev,
+    config,
+    getConfig,
     HOST: "localhost",
     PORT: "3306",
     USER: "root",
