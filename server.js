629dde87ffd53e28f18f18a3cadbfd9ebf7bdf04
corner <corner3499@gmail.com>
2022-07-11 22:06:34 +0900

    port

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(errorHandler);
 
 // 포트넘버 설정
 NODE_ENV = 'development';
-process.env.PORT = "80";
+process.env.PORT = "8080";
 process.env.JWT_SECRET = "jwt-secret-key";
 
 const PORT = process.env.PORT;
